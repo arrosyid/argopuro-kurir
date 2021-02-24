1cb9ed90f3fb82f9e02058e047e38be3a5435a1c
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-02-24 16:19:15 +0700

    +overlay scrollbar

--- application/views/templates/admin_footer.php
+++ application/views/templates/admin_footer.php
@@ -23,6 +23,8 @@
 <script src="<?= base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="<?= base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?= base_url() ?>assets/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url() ?>assets/dist/js/adminlte.min.js"></script>
 
