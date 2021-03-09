18e7ffed6494c9bfdbe001eae326746f80a0e06c
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-03-09 17:09:32 +0700

    +awal_pesanan model

--- application/models/Pesanan_model.php
+++ application/models/Pesanan_model.php
@@ -6,84 +6,47 @@ class Pesanan_model extends CI_Model
   public function getAllPesanan($type)
   {
     if ($type == 'all data') {
-      $this->db->select("pengirim.*, penerima.* , pesanan.*");
-      $this->db->from('pesanan');
-      $this->db->join('pengirim', 'pesanan.id_pengirim = pengirim.id_pengirim');
-      $this->db->join('penerima', ', pesanan.id_penerima=penerima.id_penerima');
+      $this->db->select("*")->from('awal_pesanan');
+      $this->db->order_by('date_created', 'DESC');
+      return $this->db->get()->result_array();
+    }
+    if ($type == 'pending') {
+      $this->db->select("*")->from('awal_pesanan')->where('status = 1');
       $this->db->order_by('date_created', 'DESC');
       return $this->db->get()->result_array();
     }
     if ($type == 'proses') {
-      $this->db->select("pengirim.*, penerima.* , pesanan.*");
-      $this->db->from('pesanan');
-      $this->db->join('pengirim', 'pesanan.id_pengirim = pengirim.id_pengirim');
-      $this->db->join('penerima', ', pesanan.id_penerima=penerima.id_penerima');
-      $this->db->where('status = 2');
+      $this->db->select("*")->from('awal_pesanan')->where('status = 2');
       $this->db->order_by('date_created', 'DESC');
       return $this->db->get()->result_array();
     }
     if ($type == 'diterima') {
-      $this->db->select("pengirim.*, penerima.* , pesanan.*");
-      $this->db->from('pesanan');
-      $this->db->join('pengirim', 'pesanan.id_pengirim = pengirim.id_pengirim');
-      $this->db->join('penerima', ', pesanan.id_penerima=penerima.id_penerima');
-      $this->db->where('status = 3');
+      $this->db->select("*")->from('awal_pesanan')->where('status = 3');
       $this->db->order_by('date_created', 'DESC');
       return $this->db->get()->result_array();
     }
     if ($type == 'sukses') {
-      $this->db->select("pengirim.*, penerima.* , pesanan.*");
-      $this->db->from('pesanan');
-      $this->db->join('pengirim', 'pesanan.id_pengirim = pengirim.id_pengirim');
-      $this->db->join('penerima', ', pesanan.id_penerima=penerima.id_penerima');
-      $this->db->where('status = 4');
+      $this->db->select("*")->from('awal_pesanan')->where('status = 4');
       $this->db->order_by('date_created', 'DESC');
       return $this->db->get()->result_array();
     }
   }
-  public function getAllPesananById($id)
-  {
-    $this->db->select("pengirim.*, penerima.* , pesanan.*");
-    $this->db->from('pesanan');
-    $this->db->join('pengirim', 'pesanan.id_pengirim = pengirim.id_pengirim');
-    $this->db->join('penerima', ', pesanan.id_penerima=penerima.id_penerima');
-    $this->db->order_by('date_created', 'DESC');
-    $this->db->where('pesanan', ['id_pesanan' => $id]);
-    return $this->db->get()->result_array();
-  }
   public function getPesananById($id)
   {
-    $this->db->select("pengirim.*, penerima.* , pesanan.*");
-    $this->db->from('pesanan');
-    $this->db->join('pengirim', 'pesanan.id_pengirim = pengirim.id_pengirim');
-    $this->db->join('penerima', ', pesanan.id_penerima=penerima.id_penerima');
-    $this->db->where('id_pesanan', $id);
-    return $this->db->get()->row_array();
+    return $this->db->get('awal_pesanan', ['id_pesanan' => $id])->row_array();
   }
   public function insertPesanan($data_Pesanan)
   {
-    return $this->db->insert('Pesanan', $data_Pesanan);
-  }
-  // delete all Pesanan
-  public function deletePesanan($data_Pesanan)
-  {
-    return $this->db->delete('Pesanan', $data_Pesanan);
+    return $this->db->insert('awal_pesanan', $data_Pesanan);
   }
   // delete Pesanan with type by id
-  public function deletePesananById($type = 'id_Pesanan', $param = null)
+  public function deletePesananById($param = null)
   {
-    if ($type == 'id_Pesanan')
-      return $this->db->delete('Pesanan', ['id_Pesanan' => $param]);
-
-    // if ($type == 'id_koperasi')
-    //   return $this->db->delete('Pesanan', ['id_koperasi' => $param]);
+    return $this->db->delete('awal_pesanan', ['id_Pesanan' => $param]);
   }
 
-  public function updatePesananById($type, $id, $data)
+  public function updatePesananById($id, $data)
   {
-    if ($type == 1)
-      return $this->db->update('Pesanan', $data, ['id_Pesanan' => $id]);
-    // if ($type == 2)
-    //   return $this->db->update('Pesanan', $data, ['id_koperasi' => $id]);
+    return $this->db->update('awal_pesanan', $data, ['id_Pesanan' => $id]);
   }
 }
