10a79e56155eb21f7b6bd170771c6470a62d0d4f
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-03-15 13:43:47 +0700

    fixing bug in class

--- application/libraries/Auth.php
+++ application/libraries/Auth.php
@@ -1,32 +1,34 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Auth
-{
-  function verified_access($public = false)
+if (!function_exists('test_method')) {
+  class Auth
   {
-    // menampung sesi login level user
-    $level = null;
+    function verified_access($public = false)
+    {
+      // menampung sesi login level user
+      $level = null;
 
-    // jika sudah ada session login maka simpan level login
-    if (isset($_SESSION['level'])) {
-      $level = $_SESSION['level'];
-    }
+      // jika sudah ada session login maka simpan level login
+      if (isset($_SESSION['level'])) {
+        $level = $_SESSION['level'];
+      }
 
-    // jika level 1 diarahkan ke admin
-    if ($level == 1) {
-      redirect('admin');
-    }
+      // jika level 1 diarahkan ke admin
+      if ($level == 1) {
+        redirect('admin');
+      }
 
-    // jika level 2 diarahkan ke user
-    if ($level == 2) {
-      redirect('kurir');
-    }
+      // jika level 2 diarahkan ke user
+      if ($level == 2) {
+        redirect('kurir');
+      }
 
-    // jika level null/kosong/belum login
-    if ($level == null || $level == '') {
-      // jika ingin mengakses halaman admin/user maka dikembalikan ke laman utama
-      if (!$public) {
-        redirect(base_url('auth'));
+      // jika level null/kosong/belum login
+      if ($level == null || $level == '') {
+        // jika ingin mengakses halaman admin/user maka dikembalikan ke laman utama
+        if (!$public) {
+          redirect(base_url('auth'));
+        }
       }
     }
   }
