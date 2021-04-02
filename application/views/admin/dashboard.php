3516fa47e21b6b4bc26c50ded0faf8932abfa0cb
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-04-03 01:22:39 +0700

    fixing button

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -69,9 +69,9 @@
                                     endif; ?>
                 </div>
                 <div class="col-6 ">
-                  <a href="<?= base_url('kurir/struck/' . $r['id_pesanan']) ?>" class="btn btn-primary float-right">Detail</a>
-                  <a href="<?= base_url('kurir/edit_resi/' . $r['id_pesanan']) ?>" class="btn btn-warning float-right">Edit</a>
-                  <a href="<?= base_url('kurir/delete_resi/' . $r['id_pesanan'] . '/' . $this->uri->segment(2)) ?>" class="btn btn-danger float-right">Hapus</a>
+                  <a href="<?= base_url('kurir/delete_resi/' . $r['id_pesanan'] . '/' . $this->uri->segment(2)) ?>" class="btn btn-danger pl-1 float-right">Hapus</a>
+                  <a href="<?= base_url('kurir/edit_resi/' . $r['id_pesanan']) ?>" class="btn btn-warning pl-1 float-right">Edit</a>
+                  <a href="<?= base_url('kurir/struck/' . $r['id_pesanan']) ?>" class="btn btn-primary pl-1 float-right">Detail</a>
                 </div>
                 <!-- <div class="col-3">
                   <a href="<?= base_url('kurir/struck/' . $r['id_pesanan']) ?>" class="btn btn-primary">Detail</a>
