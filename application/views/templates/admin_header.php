8ffef897a9fc9d22a6dfb1fe4e594fdbfbc1ecab
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-06-21 17:12:55 +0700

    bug#4 link home

--- application/views/templates/admin_header.php
+++ application/views/templates/admin_header.php
@@ -64,7 +64,7 @@
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="<?= base_url('kurir') ?>">Home</a></li>
                 <li class="breadcrumb-item active"><?= $title ?></li>
               </ol>
             </div>
